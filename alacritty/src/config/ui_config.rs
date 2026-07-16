759a709680b0f0218080ecc721bed5c41780699b
Vyacheslav Dryamov <Vyacheslav.Dryamov@infowatch.com>
2026-07-16 09:28:27 +0300

    Revert "feat: pane_margin config — visual gap between split panes"
    This reverts commit 9f666a32484933466cfbe190a725ce334eb34ec9.

--- alacritty/src/config/ui_config.rs
+++ alacritty/src/config/ui_config.rs
@@ -92,10 +92,6 @@ pub struct UiConfig {
     /// Regex hints for interacting with terminal content.
     pub hints: Hints,
 
-    /// Pane margin in pixels.
-    #[config(default = "4.0")]
-    pub pane_margin: f32,
-
     /// Config for the alacritty_terminal itself.
     pub terminal: Terminal,
 
